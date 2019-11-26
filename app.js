55b2c41079caaef93fe6972dafa09fa785a535a9
LocPerseus <kinghoangloc@gmail.com>
2019-11-26 17:06:59 +0700

    restapi using data JSON

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 
